fde2c37db1274e8a8e15cdab21977bc545535be5
Nithya Swaminathan <nithyaswam@hotmail.com>
2018-03-27 13:34:59 +0530

    Update iis.rb
    added certificate

--- packs/iis.rb
+++ packs/iis.rb
@@ -39,7 +39,7 @@ resource "iis-website",
   :requires     => {
     :constraint => "1..1",
     :help       => "Installs/Configure IIS",
-    :services => "*dotnet-platform"
+    :services => "*dotnet-platform,*certificate"
   },
   :attributes   => {
     "package_name"                      => '',
